99385305263eea3d6575411ad8707e88fbe2a12a
MaksJS <maxigamer59@hotmail.com>
2013-05-11 23:05:34 +0200

    test

--- app/Global.scala
+++ app/Global.scala
@@ -15,7 +15,7 @@ object Global extends GlobalSettings {
 	override def onStart(app: Application) {
 		if(User.find.all.size == 0) {
 			val user = new User(1L, "admin", "fc312767fb4c127a8d2759a5e513b03a33483b21", "admin@aeroclub.git")
-			user.save()
+			user.save
 			Account.create(user)
 		}
 	}
