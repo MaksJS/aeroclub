b49e224a4bafa51429ebbd3d1dd7b4b8bf51a047
MaksJS <maxigamer59@hotmail.com>
2013-05-11 22:25:13 +0200

    scalaify indent

--- app/models/Account.scala
+++ app/models/Account.scala
@@ -6,7 +6,7 @@ import anorm.SqlParser._
 import play.api.Play.current
 
 case class Account(id: Pk[Long], amount: Double, userId: Long) {
-	def transactions: Seq[Transaction] = {
+  def transactions: Seq[Transaction] = {
     DB.withConnection { implicit c =>
       SQL(
         """
@@ -23,37 +23,37 @@ case class Account(id: Pk[Long], amount: Double, userId: Long) {
 
 object Account {
 
-	val account = {
-		get[Pk[Long]]("id")~
-		get[Double]("amount")~
-		get[Long]("user_id") map {
-			case id~amount~user_id => Account(id, amount, user_id)
-		}
-	}
+  val account = {
+    get[Pk[Long]]("id")~
+    get[Double]("amount")~
+    get[Long]("user_id") map {
+      case id~amount~user_id => Account(id, amount, user_id)
+    }
+  }
 
-	def create(user: User) {
-		DB.withConnection { implicit c =>
-			SQL(
-				"""
-					INSERT INTO account(amount, user_id) VALUES ({amount}, {user_id})
-				"""
-			).on(
-				'amount  -> 0.0, 
-				'user_id -> user.id
-			).executeInsert()
-		}
-	}
+  def create(user: User) {
+    DB.withConnection { implicit c =>
+      SQL(
+        """
+          INSERT INTO account(amount, user_id) VALUES ({amount}, {user_id})
+        """
+      ).on(
+        'amount  -> 0.0, 
+        'user_id -> user.id
+      ).executeInsert()
+    }
+  }
 
-	def getByUser(user: User): Account = {
-		DB.withConnection { implicit c =>
-			SQL(
-				"""
-					SELECT * FROM account
-					WHERE user_id = {user_id}
-				"""
-			).on(
-				'user_id -> user.id
-			).as(account.single)
-		}
-	}
+  def getByUser(user: User): Account = {
+    DB.withConnection { implicit c =>
+      SQL(
+        """
+          SELECT * FROM account
+          WHERE user_id = {user_id}
+        """
+      ).on(
+        'user_id -> user.id
+      ).as(account.single)
+    }
+  }
 }
